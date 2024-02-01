37688dfc08bea6e551604e8ed361e1a020856103
mateuszpiorowski <mateuszpiorowski@gmail.com>
2024-02-01 15:57:58 +0100

    update

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -4,17 +4,13 @@
     const articles = data.articles;
 </script>
 
-<h1 class="text-2xl">Welcome to SvelteKit</h1>
-<p class="mt-2 text-black">
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a>
-    to read the documentation
-</p>
-
-<ul class="mt-6 list-decimal text-black">
-    <h2 class="text-xl">Articles</h2>
-    {#each articles as article}
-        <li>
-            {article.title}
-        </li>
-    {/each}
-</ul>
+<main class="max-w-6xl mx-auto">
+    <ul class="mt-6 list-disc list-inside">
+        <h2 class="text-xl">Articles</h2>
+        {#each articles as article}
+            <li>
+                {article.title}
+            </li>
+        {/each}
+    </ul>
+</main>
