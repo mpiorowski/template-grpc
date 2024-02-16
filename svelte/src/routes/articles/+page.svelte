1c0afc3226b3d3e1b2167ef857ae7e20f9f8cb4e
mateuszpiorowski <mateuszpiorowski@gmail.com>
2024-02-16 10:55:02 +0100

    update

--- svelte/src/routes/articles/+page.svelte
+++ svelte/src/routes/articles/+page.svelte
@@ -4,11 +4,12 @@
     const articles = data.articles;
 </script>
 
-<ul class="mt-6 list-inside list-disc">
-    <h2 class="text-xl text-gray-900">Articles</h2>
+<ul class="mt-6 max-w-4xl list-outside list-disc">
+    <h2 class="text-xl">Articles</h2>
     {#each articles as article}
         <li>
             {article.title}
+            <div class="rounded-xl border p-4">{@html article.content}</div>
         </li>
     {/each}
 </ul>
