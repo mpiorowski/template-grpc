ad01e360639ffd6a5978eb1fe65af3186bc5e2c6
mateuszpiorowski <mateuszpiorowski@gmail.com>
2024-02-24 20:03:43 +0100

    update

---
+++ go/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"log/slog"
+	"net"
+	"net/http"
+	"os"
+	"powerit/db"
+	"powerit/users"
+	"powerit/utils"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/lmittmann/tint"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	pb "powerit/proto"
+)
+
+type server struct {
+	pb.UnimplementedUsersServiceServer
+}
+
+func main() {
+	// Set up the logger
+	w := os.Stderr
+    var log slog.Level
+    if utils.LOG_LEVEL == "info" {
+        log = slog.LevelInfo
+    } else {
+        log = slog.LevelDebug
+    }
+	slog.SetDefault(slog.New(
+		tint.NewHandler(w, &tint.Options{
+            AddSource: true,
+			Level:      log,
+			TimeFormat: time.Kitchen,
+		}),
+	))
+
+	// Connect to the database
+	err := db.Connect()
+	if err != nil {
+		slog.Error("Error opening database", "db.Connect", err)
+		panic(err)
+	}
+	slog.Info("Database connected")
+
+	// Run migrations
+	err = db.Migrations()
+	if err != nil {
+		slog.Error("Error running migrations", "db.Migrations", err)
+		panic(err)
+	}
+	slog.Info("Migrations completed")
+
+	// Run the gRPC server
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%v", utils.GRPC_PORT))
+	if err != nil {
+		slog.Error("Error listening on gRPC port", "net.Listen", err)
+		panic(err)
+	}
+	var s *grpc.Server
+	if utils.TLS == "true" {
+		certificate, err := tls.LoadX509KeyPair(utils.CERT_PATH, utils.KEY_PATH)
+		if err != nil {
+			slog.Error("Error loading TLS certificate", "tls.LoadX509KeyPair", err)
+			panic(err)
+		}
+		s = grpc.NewServer(grpc.Creds(credentials.NewServerTLSFromCert(&certificate)))
+	} else {
+		s = grpc.NewServer()
+	}
+	pb.RegisterUsersServiceServer(s, &server{})
+	go func() {
+		slog.Info("gRPC server listening on", "port", utils.GRPC_PORT)
+		err = s.Serve(lis)
+		if err != nil {
+			slog.Error("Error serving gRPC", "s.Serve", err)
+			panic(err)
+		}
+	}()
+
+	// Run the HTTP server
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		c.Response().Header().Set("Access-Control-Allow-Origin", utils.CLIENT_URL)
+		id := 0
+		err := db.Db.QueryRow("SELECT 1").Scan(&id)
+		if err != nil {
+			slog.Error("Error pinging database", "Db.QueryRow", err)
+			return c.String(http.StatusInternalServerError, "Error pinging database")
+		}
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.GET("/oauth-login/:provider", func(c echo.Context) error {
+		return users.OauthLogin(c)
+	})
+	e.GET("/oauth-callback/:provider", func(c echo.Context) error {
+		return users.OauthCallback(c)
+	})
+	go func() {
+		slog.Info("HTTP server listening on", "port", utils.HTTP_PORT)
+		if utils.TLS == "true" {
+			err = e.StartTLS(":"+utils.HTTP_PORT, utils.CERT_PATH, utils.KEY_PATH)
+		} else {
+			err = e.Start(":" + utils.HTTP_PORT)
+		}
+		if err != nil {
+			slog.Error("Error serving HTTP", "e.Start", err)
+			panic(err)
+		}
+	}()
+
+	select {}
+}
---
+++ go/users/user_http.go
@@ -0,0 +1,123 @@
+package users
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"powerit/utils"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"github.com/redis/go-redis/v9"
+	"golang.org/x/oauth2"
+)
+
+/**
+ *  Oauth login
+ *  @api {get} /oauth-login/:provider Oauth login
+ */
+func OauthLogin(c echo.Context) error {
+	config, err := OAuthConfig.getOAuthConfig(c.Param("provider"))
+	if err != nil {
+		slog.Error("Error getting provider", "configProvider.getOAuthConfig", err)
+		return c.Redirect(http.StatusTemporaryRedirect, utils.CLIENT_URL+"/auth?error=unauthorized")
+	}
+
+	// generate random state and verifier
+	state := utils.GenerateRandomState(32)
+	verifier := oauth2.GenerateVerifier()
+	// store state and verifier
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     utils.REDIS_URL,
+		Password: utils.REDIS_PASSWORD,
+	})
+	err = rdb.Set(context.Background(), state, verifier, 5*time.Minute).Err()
+    if err != nil {
+        slog.Error("Error setting state and verifier", "rdb.Set", err)
+        return c.Redirect(http.StatusTemporaryRedirect, utils.CLIENT_URL+"/auth?error=unauthorized")
+    }
+	// Redirect user to consent page to ask for permission
+	url := config.AuthCodeURL(state, oauth2.AccessTypeOffline, oauth2.S256ChallengeOption(verifier))
+	return c.Redirect(http.StatusTemporaryRedirect, url)
+}
+
+/**
+ *  Oauth callback
+ *  @api {get} /oauth-callback/:provider Oauth callback
+ */
+func OauthCallback(c echo.Context) error {
+	provider := c.Param("provider")
+	code := c.QueryParam("code")
+	state := c.QueryParam("state")
+
+	// get verifier from state
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     utils.REDIS_URL,
+		Password: utils.REDIS_PASSWORD,
+	})
+	verifier, err := rdb.Get(context.Background(), state).Result()
+	if err != nil {
+		slog.Error("Error getting verifier", "rdb.Get", err)
+		return c.Redirect(http.StatusTemporaryRedirect, utils.CLIENT_URL+"/auth?error=unauthorized")
+	}
+
+	// get oauth config
+	config, err := OAuthConfig.getOAuthConfig(provider)
+	if err != nil {
+		slog.Error("Error getting provider", "configProvider.getOAuthConfig", err)
+		return c.Redirect(http.StatusTemporaryRedirect, utils.CLIENT_URL+"/auth?error=unauthorized")
+	}
+
+	// get oauth token
+	oauthToken, err := config.Exchange(context.Background(), code, oauth2.VerifierOption(verifier))
+	if err != nil {
+		slog.Error("Error exchanging code for token", "config.Exchange", err)
+		return c.Redirect(http.StatusTemporaryRedirect, utils.CLIENT_URL+"/auth?error=unauthorized")
+	}
+
+	// fetch user info from github
+	userInfo, err := OAuthConfig.getUserInfo(provider, oauthToken.AccessToken)
+	if err != nil {
+		slog.Error("Error fetching user info", "configProvider.getUserInfo", err)
+		return c.Redirect(http.StatusTemporaryRedirect, utils.CLIENT_URL+"/auth?error=unauthorized")
+	}
+
+	// get user, create if not exists
+	user, err := selectUserByEmailAndSub(userInfo.email, userInfo.sub)
+	if err != nil {
+		user, err = insertUser(userInfo.email, userInfo.sub, userInfo.avatar)
+		if err != nil {
+			slog.Error("Error inserting user", "insertUser", err)
+			return c.Redirect(http.StatusTemporaryRedirect, utils.CLIENT_URL+"/auth?error=invalid_user")
+		}
+	}
+
+	// create oauth token with a 7 day expiration
+	tokenId, err := uuid.NewV7()
+	if err != nil {
+		slog.Error("Error creating token id", "uuid.NewV7", err)
+		return c.Redirect(http.StatusTemporaryRedirect, utils.CLIENT_URL+"/auth?error=unauthorized")
+	}
+	err = rdb.Set(context.Background(), tokenId.String(), user.Id, 7*24*time.Hour).Err()
+    if err != nil {
+        slog.Error("Error setting token id", "rdb.Set", err)
+        return c.Redirect(http.StatusTemporaryRedirect, utils.CLIENT_URL+"/auth?error=unauthorized")
+    }
+
+	// set cookie
+	cookie := &http.Cookie{}
+	cookie.Domain = utils.COOKIE_DOMAIN
+	cookie.Name = "token"
+	cookie.Value = tokenId.String()
+	cookie.Path = "/"
+	cookie.Secure = true
+	cookie.SameSite = http.SameSiteLaxMode
+	cookie.HttpOnly = true
+	// 7 days
+	cookie.MaxAge = 7 * 24 * 60 * 60
+	c.SetCookie(cookie)
+
+	// redirect to home page
+	return c.Redirect(http.StatusTemporaryRedirect, utils.CLIENT_URL)
+}
