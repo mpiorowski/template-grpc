024390d787764386f980f757a3ce7147ccdfec42
mateuszpiorowski <mateuszpiorowski@gmail.com>
2024-02-25 00:12:09 +0100

    update

--- proto.sh
+++ proto.sh
@@ -1,9 +1,3 @@
-# Client
-rm -rf ./client/src/lib/proto
-mkdir ./client/src/lib/proto
-# sudo npm -g i @grpc/proto-loader
-proto-loader-gen-types --keepCase --longs=String --enums=String --defaults --oneofs --grpcLib=@grpc/grpc-js --outDir=./client/src/lib/proto ./proto/*.proto && cp ./proto/*.proto ./client/src/lib/proto/
-
 # Service Users
 rm -rf ./service-users/proto
 mkdir ./service-users/proto
@@ -12,3 +6,7 @@ protoc --go_out=./service-users/proto --go_opt=paths=source_relative \
     --proto_path=./proto \
     ./proto/*.proto
 
+# Client
+rm -rf ./client/src/lib/proto
+mkdir ./client/src/lib/proto
+proto-loader-gen-types --keepCase --longs=String --enums=String --defaults --oneofs --grpcLib=@grpc/grpc-js --outDir=./client/src/lib/proto ./proto/*.proto && cp ./proto/*.proto ./client/src/lib/proto/
