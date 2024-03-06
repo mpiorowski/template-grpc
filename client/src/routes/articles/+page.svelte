f3759af8824266520ad961741a36bff275bb6f82
mateuszpiorowski <mateuszpiorowski@gmail.com>
2024-03-06 22:55:06 +0100

    update

--- client/src/routes/articles/+page.svelte
+++ client/src/routes/articles/+page.svelte
@@ -1,8 +1,29 @@
 <script>
     import { PUBLIC_DIRECTUS_URL } from "$env/static/public";
+    import { onMount } from "svelte";
+    import { ScrollSmoother, ScrollTrigger, gsap } from "$lib/gsap";
+
     /** @type {import("./$types").PageData} */
     export let data;
     const articles = data.articles;
+
+    onMount(() => {
+        ScrollSmoother.create({
+            smooth: 1,
+            effects: false,
+        });
+
+        ScrollTrigger.refresh();
+        gsap.matchMedia().add("(min-width: 768px)", () => {
+            const tl = gsap.timeline();
+            tl.from("#articles > ul > li", {
+                y: 200,
+                opacity: 0,
+                duration: 2,
+                ease: "power4.out",
+            });
+        });
+    });
 </script>
 
 <div class="min-h-full bg-gray-900 font-poppins text-gray-50 antialiased">
@@ -12,7 +33,7 @@
             <a href="/profile" class="text-gray-50">Profile</a>
         </nav>
     </header>
-    <main class="p-10">
+    <main class="p-10" id="articles">
         <ul class="max-w-4xl list-outside list-disc">
             <h2 class="mb-10 text-xl">Articles</h2>
             {#each articles as article}
