3e38968f1c5bdce66f08659073d1a4b67d266b82
mateuszpiorowski <mateuszpiorowski@gmail.com>
2024-03-08 11:12:29 +0100

    update

--- client/src/routes/(app)/nav.svelte
+++ client/src/routes/(app)/nav.svelte
@@ -80,7 +80,41 @@
                     Contact
                 </a>
             </li>
+            <li>
+                <a
+                    on:click={close}
+                    href="/billing"
+                    class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6
+                    {current === 'billing'
+                        ? 'bg-gray-800 text-white'
+                        : 'text-gray-400 hover:bg-gray-800 hover:text-white'}"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        class="feather feather-credit-card"
+                    >
+                        <rect
+                            x="1"
+                            y="4"
+                            width="22"
+                            height="16"
+                            rx="2"
+                            ry="2"
+                        ></rect>
+                        <line x1="1" y1="10" x2="23" y2="10"></line>
+                    </svg>
 
+                    Billing
+                </a>
+            </li>
             <li>
                 <a
                     on:click={close}
